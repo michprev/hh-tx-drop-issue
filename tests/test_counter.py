a3c572d6bb8820482b998c86f9912ebb60aeca7f
Michal Převrátil <michprev@gmail.com>
2022-12-08 15:35:34 +0100

    ✨ Add `get_block("pending")` calls

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -3,6 +3,10 @@ from woke.testing.random import random_address
 
 from pytypes.contracts.Counter import Counter
 
+from woke.cli.console import console
+
+import json
+
 
 @connect(default_chain, "http://localhost:8545")
 @snapshot_and_revert(default_chain)
@@ -17,6 +21,8 @@ def test_counter():
         tx1 = counter.increment(return_tx=True)
         tx2 = counter.increment(return_tx=True)
 
+        console.print_json(json.dumps(default_chain.dev_chain.get_block("pending")))
         default_chain.mine()
+        console.print_json(json.dumps(default_chain.dev_chain.get_block("pending")))
     
     assert counter.count() == 2
